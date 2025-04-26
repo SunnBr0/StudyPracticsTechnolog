7cad89e760d7bdb3655298f495fc85c4c09bc2b9
Vladimir <vova.voronov.2002@bk.ru>
2025-04-26 15:25:06 +0300

    new feature unsubscribe University

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ package src;
 public class Main {
     public static void main(String[] args) {
         // Постановка задачи:
-        // Создать систему, в которой можно было бы регистрировать преподов, студентов и курсы
+        // Создать систему, в которой можно было бы регистрировать преподов, студентов и
+        // курсы
         University university = new University("ВГУ");
 
         Teacher teacher1 = new Teacher("Иван Иванов Иванович", 1);
@@ -11,23 +12,40 @@ public class Main {
 
         Course course1 = new Course("Дискретная математика", "MATH101", teacher1);
         Course course2 = new Course("Физика", "PHYS101", teacher2);
+        Course course3 = new Course("Химия", "CHIM101", teacher2);
+        Course course4 = new Course("Инфомартика", "INFO101", teacher2);
 
         Student student1 = new Student("Вупсень", 1);
         Student student2 = new Student("Пупсень", 2);
 
         university.addTeacher(teacher1);
         university.addTeacher(teacher2);
+
         university.addCourse(course1);
         university.addCourse(course2);
+        university.addCourse(course3);
+        university.addCourse(course4);
+
         university.addStudent(student1);
         university.addStudent(student2);
-        university.enrollStudentInCourse(student1, course1);
-        university.enrollStudentInCourse(student2, course2);
-        university.assignTeacherToCourse(teacher1, course1);
-        university.assignTeacherToCourse(teacher2, course2);
 
-        university.getInfoStudentInCourse();
-        university.getInfoTeacherToCourse();
+        university.enrollStudentInCourse(student1, course1);
+        university.enrollStudentInCourse(student1, course2);
+        university.enrollStudentInCourse(student2, course2);
+        university.enrollStudentInCourse(student2, course3);
+
+        university.assignTeacherToCourse(teacher1, course1);
+        university.assignTeacherToCourse(teacher1, course2);
+        university.assignTeacherToCourse(teacher1, course3);
+        university.assignTeacherToCourse(teacher2, course2);
+        university.assignTeacherToCourse(teacher2, course3);
+
+        university.getInfoTeachersAndStudents();
+
+        university.unsubscribeStudentInCourse(student2, course3);
+        university.cancelTeacherToCourse(teacher2, course3);
+        
+        university.getInfoTeachersAndStudents();
 
     }
 
--- src/University.java
+++ src/University.java
@@ -1,4 +1,5 @@
 package src;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -7,6 +8,7 @@ public class University {
     private List<Teacher> teachers;
     private List<Course> courses;
     private String name;
+
     public University(String name) {
         this.name = name;
         this.students = new ArrayList<>();
@@ -25,29 +27,42 @@ public class University {
     public void addCourse(Course course) {
         courses.add(course);
     }
-    public String getName(){
+
+    public String getName() {
         return name;
     }
+
     public void enrollStudentInCourse(Student student, Course course) {
         student.enrollCourse(course);
+    }
+
+    public void unsubscribeStudentInCourse(Student student, Course course) {
+        student.unsubscribeCourse(course);
     }
 
     public void assignTeacherToCourse(Teacher teacher, Course course) {
         teacher.assignCourse(course);
     }
 
-    public void getInfoStudentInCourse(){
+    public void cancelTeacherToCourse(Teacher teacher, Course course) {
+        teacher.cancelCourse(course);
+    }
+
+    public void getInfoStudentInCourse() {
         for (Student student : students) {
             System.out.println("Университет: " + name);
+            System.out.println("Студент ");
             System.out.println("Имя: " + student.getName());
             System.out.println("Прикреплён к курсам: " + student.getEnrolledCourses());
             System.out.println("Идентификатор студента: " + student.getId());
             System.out.println();
         }
     }
-    public void getInfoTeacherToCourse(){
+
+    public void getInfoTeacherToCourse() {
         for (Teacher teacher : teachers) {
             System.out.println("Университет: " + name);
+            System.out.println("Препод ");
             System.out.println("Имя: " + teacher.getName());
             System.out.println("Преподаёт курсы: " + teacher.getTeachingCourses());
             System.out.println("Идентификатор учителя: " + teacher.getId());
@@ -55,5 +70,24 @@ public class University {
         }
     }
 
-}
+    public void getInfoTeachersAndStudents() {
+        System.out.println("=======================================================");
+        System.out.println("Университет: " + name);
+        for (Student student : students) {
+            System.out.println("Студент ");
+            System.out.println("Имя: " + student.getName());
+            System.out.println("Прикреплён к курсам: " + student.getEnrolledCourses());
+            System.out.println("Идентификатор студента: " + student.getId());
+            System.out.println();
+        }
+        for (Teacher teacher : teachers) {
+            System.out.println("Препод ");
+            System.out.println("Имя: " + teacher.getName());
+            System.out.println("Преподаёт курсы: " + teacher.getTeachingCourses());
+            System.out.println("Идентификатор учителя: " + teacher.getId());
+            System.out.println();
+        }
+        System.out.println("=======================================================");
+    }
 
+}
