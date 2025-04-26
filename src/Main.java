e5d3cb09063c7437ec10b360f8ad31d26810bca3
Vladimir <vova.voronov.2002@bk.ru>
2025-04-26 15:21:24 +0300

    add homework

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,7 @@ public class Main {
         university.assignTeacherToCourse(teacher1, course1);
         university.assignTeacherToCourse(teacher2, course2);
 
+        
         university.getInfoStudentInCourse();
         university.getInfoTeacherToCourse();
 
